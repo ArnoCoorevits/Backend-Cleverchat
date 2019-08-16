a5819cd9e393d5d2b20c39ed84991c8e335eb487
ArnoCoorevits <arno.coorevits.v6069@student.hogent.be>
2019-08-16 16:44:47 +0200

    update

--- models/Chatbot.js
+++ models/Chatbot.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose'), Schema = mongoose.Schema
 const chatbotSchema = new Schema({
     name: String,
     categorie: String,
-    image: String
+    image: String,
+    apiKey: String,
+    endPoint: String,
+    assistantId: String
 })
 
 mongoose.model("Chatbots", chatbotSchema, "Chatbots")
--- server.js
+++ server.js
@@ -10,22 +10,15 @@ var passport = require('passport');
 var bodyParser = require('body-parser');
 var cors = require('cors')
 
-//Load mongoose models
 require('./models/User')
 require('./models/Chatbot')
 
-//Load express routers
 var indexRouter = require('./routes/index.routes');
 var usersRouter = require('./routes/user.routes');
 var chatbotsRouter = require('./routes/chatbots.routes');
 
 var app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
-//Load libraries
 app.use(logger('dev'));
 app.use(cors())
 app.use(express.json());
@@ -36,14 +29,12 @@ app.use(passport.initialize());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//Express router setup
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
 app.use('/chatbots', chatbotsRouter);
 
-//Mongoose setup
 mongoose.connect(
-  'mongodb://localhost:27017/PizzaDB',
+  `mongodb+srv://arnocoorevits:${process.env.MONGO_PASSWORD}@cluster0-0ccgj.mongodb.net/cleverchat`,
   { useCreateIndex: true, useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, '[Mongoose] Connection error:'));
@@ -51,18 +42,13 @@ db.once('open', function() {
   console.info('[Mongoose] Connected to database')
 });
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.json({error: err})
 });
